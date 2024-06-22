4bc5824a4f061c8549894784b2582afc4ff2be06
marten-sova <martensova@gmail.com>
2024-06-21 17:45:48 -0700

    allow empty zones

--- server/controllers/warehouse/create.js
+++ server/controllers/warehouse/create.js
@@ -1,5 +1,5 @@
-const joi = require('joi')
-const Warehouse = require('../../models/Warehouse')
+const joi = require("joi");
+const Warehouse = require("../../models/Warehouse");
 
 async function create(request, response, next) {
   try {
@@ -7,49 +7,57 @@ async function create(request, response, next) {
     await joi
       .object({
         name: joi.string().required(),
-        zones: joi.array().max(12).items(
-          joi.object({
-            zoneNumber: joi.number().required(),
-            shelves: joi.array().max(10).items(
-              joi.object({
-                name: joi.string().required(),
-              }).required(),
-            ),
-          }).required(),
-        )
+        zones: joi
+          .array()
+          .max(12)
+          .items(
+            joi
+              .object({
+                zoneNumber: joi.number().required(),
+                shelves: joi
+                  .array()
+                  .max(10)
+                  .items(
+                    joi.object({
+                      name: joi.string().required(),
+                    })
+                  ),
+              })
+              .required()
+          ),
       })
-      .validateAsync(request.body)
+      .validateAsync(request.body);
   } catch (error) {
     return response.status(400).json({
-      error: 'ValidationError',
+      error: "ValidationError",
       message: error.message,
-    })
+    });
   }
 
   try {
-    const {name, zones} = request.body
+    const { name, zones } = request.body;
 
     // Verify warehouse name is unique
-    const existingWarehouse = await Warehouse.findOne({name})
+    const existingWarehouse = await Warehouse.findOne({ name });
     if (existingWarehouse) {
       return response.status(400).json({
         error: name,
         message: 'A warehouse already exists with that "name"',
-      })
+      });
     }
 
     // Create warehouse
-    const newWarehouse = new Warehouse({name, zones})
-    await newWarehouse.save()
+    const newWarehouse = new Warehouse({ name, zones });
+    await newWarehouse.save();
 
     response.status(201).json({
-      message: 'Succesfully created warehouse',
+      message: "Succesfully created warehouse",
       data: newWarehouse,
-    })
+    });
   } catch (error) {
-    console.error(error)
-    return response.status(500).send()
+    console.error(error);
+    return response.status(500).send();
   }
 }
 
-module.exports = create
+module.exports = create;
